0535cc875c1bd2d2f2ab634f67b6b42e29c209a0
Shyju krishnan <62458394+shyjustack@users.noreply.github.com>
2026-03-15 21:02:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Flask on Azure Prajeesh!!!</h1><p>Successfully deployed.</p>"
+    return "<h1>Hello from Flask on Azure !</h1><p>Successfully deployed.</p>"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
